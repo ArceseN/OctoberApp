f9b217d6f88cf789c1dd38572dff2d3ef2d736e4
nadia arcese <nadiaarcese@gmail.com>
2019-10-24 18:00:34 -0400

    first commit

---
+++ src/test/java/com/nadiaworks/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadiaworks.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
